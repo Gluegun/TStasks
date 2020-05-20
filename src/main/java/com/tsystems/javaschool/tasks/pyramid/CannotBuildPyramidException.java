e7f8925eb520999afc176f9bbcaa9921308de0e7
Gluegun <kirja5@mail.ru>
2020-05-20 10:27:03 +0300

    task is done

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/CannotBuildPyramidException.java
@@ -1,4 +1,8 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
 public class CannotBuildPyramidException extends RuntimeException {
+    @Override
+    public String getMessage() {
+        return "Impossible to create pyramid";
+    }
 }
